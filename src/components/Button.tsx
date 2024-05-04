c62d4c188af281ed04f49245e7b1797593b82748
andressaaborges <emylleaborges@gmail.com>
2024-05-04 20:35:09 -0300

    style: :lipstick: add global styles with `styled-components`

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import { ButtonContainer, ButtonVariant } from './Button.styles';
+
+interface ButtonProps {
+    readonly variant?: ButtonVariant;
+}
+
+export function Button({ variant = 'primary' }: ButtonProps) {
+    return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
+}
