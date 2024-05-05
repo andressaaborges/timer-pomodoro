33bf03e962421351c173a37f57467ec97506bca6
andressaaborges <emylleaborges@gmail.com>
2024-05-04 22:50:01 -0300

    feat: :sparkles: add routes white `react-router-dom`

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVariant } from './Button.styles'
-
-interface ButtonProps {
-  readonly variant?: ButtonVariant
-}
-
-export function Button({ variant = 'primary' }: ButtonProps) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
